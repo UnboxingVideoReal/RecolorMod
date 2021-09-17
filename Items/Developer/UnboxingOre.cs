9fb16e65d5e0384a1be9bc1c64f912b964a96e8a
UnboxingVideoReal <88110899+UnboxingVideoReal@users.noreply.github.com>
2021-09-16 21:52:06 -0500

    add unboxing ore

---
+++ Items/Developer/UnboxingOre.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria;
+
+namespace RecolorMod.Items.Developer
+{
+	public class UnboxingOre : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Unboxing Ore");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+			ItemID.Sets.SortingPriorityMaterials[Item.type] = 58;
+		}
+
+		public override void SetDefaults() {
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTurn = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.autoReuse = true;
+			Item.maxStack = 999;
+			Item.consumable = true;
+			Item.rare = ModContent.RarityType<Rarities.Developer>();
+			Item.createTile = ModContent.TileType<Tiles.Developer.UnboxingOre>();
+			Item.width = 12;
+			Item.height = 12;
+			Item.value = Item.buyPrice(50, 1, 500, 900);
+		}
+
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+			Item.color = new Color(Main.DiscoR, 0, Main.DiscoB);
+		}
+
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+		{
+			Item.color = new Color(Main.DiscoR, 0, Main.DiscoB);
+		}
+	}
+}
