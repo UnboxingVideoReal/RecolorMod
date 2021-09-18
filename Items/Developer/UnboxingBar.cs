ef110994e623fa733cefe127ad06f15015501832
Calamitz <notunboxing6@gmail.com>
2021-09-17 20:14:38 -0500

    i think i fixed it

--- Items/Developer/UnboxingBar.cs
+++ Items/Developer/UnboxingBar.cs
@@ -4,6 +4,7 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 
 namespace RecolorMod.Items.Developer
 {
@@ -11,6 +12,7 @@ namespace RecolorMod.Items.Developer
 	{
 		public override void SetStaticDefaults() {
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(8, 8));
 			ItemID.Sets.SortingPriorityMaterials[Item.type] = 100; // Influences the inventory sort order. 59 is PlatinumBar, higher is more valuable.
 		}
 
@@ -26,7 +28,7 @@ namespace RecolorMod.Items.Developer
 			Item.autoReuse = true;
 			Item.rare = ModContent.RarityType<Rarities.Developer>();
 			Item.consumable = true;
-			Item.createTile = ModContent.TileType<Tiles.Developer.UnboxingBar>(); // The ID of the wall that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets)..
+/*			Item.createTile = ModContent.TileType<Tiles.Developer.UnboxingBar>();*/ // The ID of the wall that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets)..
 			Item.placeStyle = 0;
 		}
 		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
@@ -41,14 +43,11 @@ namespace RecolorMod.Items.Developer
 		public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
 		{
 			Item.color = new Color(Main.DiscoR, 0, Main.DiscoB);
-			Texture2D texture = ModContent.GetTexture("RecolorMod/Items/Developer/UnboxingBar_Animated");
-			spriteBatch.Draw(texture, position, ((ModItem)this).get_item().GetCurrentFrame(ref frame, ref frame, 8, 8), Color.White, 0f, origin, scale, SpriteEffects.None, 0f);
 		}
 
 		public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
 		{
-			Texture2D texture = ModContent.GetTexture("RecolorMod/Items/Developer/UnboxingBar_Animated");
-			spriteBatch.Draw(texture, (Item.position - Main.screenPosition, ((ModItem)this).get_item().GetCurrentFrame(ref frame, ref frameCounter, 8, 8), lightColor, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			Item.color = new Color(Main.DiscoR, 0, Main.DiscoB);
 		}
 	}
 }
